770f2d8fe39ad6ffe7ed3164d116f900c8c784b3
eganal <alex.egan13@icloud.com>
2021-02-01 21:36:44 -0800

    New File

---
+++ PoleProject/Functions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PoleProject
+{
+    public class Functions
+    {
+        public Functions()
+        {
+        }
+
+        public void randomStatement()
+        {
+            Console.WriteLine("Yo.");
+        }
+    }
+}
